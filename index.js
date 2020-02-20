507965ed204bdd80206b3d476354f6c843ebb5e0
unknown <haidari.ali20145@gmail.com>
2020-02-20 16:22:54 +0100

    The fruits list ar sorted

--- index.js
+++ index.js
@@ -22,7 +22,20 @@ const renderFruitSelect = (fruitSelect) => {
 			const kies = document.createElement("option")
 			kies.text = 'kies'
 			fruitSelect.add(kies, null)
-			const fruits = result.sort()
+			const fruits = result.sort(function (a, b) {
+				var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+				var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+				if (nameA < nameB) {
+					return -1;
+				}
+				if (nameA > nameB) {
+					return 1;
+				}
+
+				// names must be equal
+				return 0;
+			})
+			console.log(fruits)
 			for (const fruit of fruits) {
 				const option = document.createElement("option")
 				option.text = fruit.name
